fix data normalization bug

DataManager.py
@@ -216,7 +216,7 @@ class DataManager():
         returns a list of data cells of format([np.array(bsdata), tsdata, rtdata, lbdata])
         """
 
-        print "[ create_dataset ]... "
+        print "[ create_dataset simple]... "
         data_all = []
         bsset = self.get_bsdata()[bfeatures]
         bsset = bsset[bsset['pb'] > 0]
@@ -230,15 +230,16 @@ class DataManager():
             if days is None:
                 data_stock = stockset[symb][tsfeatures]
             else:
-                data_stock = stockset[symb][tsfeatures][-days:]
+                data_stock = stockset[symb][tsfeatures][-days-lookback:]
             datelist = mydate(list(data_stock.index))
             datecol = np.array(intdate(datelist)).reshape(-1,1)
             bsdata = bsset[bsset[:,0]==int(symb)][0]  # sym,...
-            for i in range(len(data_stock) - lookback - 1):
-                if data_stock['p_change'][i + lookback] >9.98: continue  # clean data un-operational
+            for i in range(len(data_stock) - lookback):
+                if data_stock['p_change'][i + lookback-1] >=9.96: continue  # clean data un-operational
                 dtcell = np.array(data_stock)[i:(i+lookback+1)]
-                ohcl = minmax_scale(dtcell[:,0:4])
-                tsdata = np.hstack([datecol[i: i+lookback], ohcl[:-1], dtcell[:-1, 4:]])
+                ohcl = minmax_scale(dtcell[:-1,0:4])
+                # 应该直对testcase进行normalization
+                tsdata = np.hstack([datecol[i: i+lookback], ohcl, dtcell[:-1, 4:]])
                 # tsdata_v = np.hstack([datecol[i: i+lookback], dtcell[:-1,:]])
                 # rtdata = np.hstack([[int(symb)], datecol[i+lookback], ohcl[-1], dtcell[-1, 4:]])
                 # rtdata_v = np.hstack([[int(symb)], datecol[i+lookback], dtcell[-1]])
@@ -276,7 +277,7 @@ class DataManager():
         :param dataset: source dataset, a list of data cell of [bsdata, tsdata, rtdata, lbdata, tsdata_v, rtdata_v, lbdata_v]
         :return: tuple of (bsdata, tsdata, rtdata, lbdata, tsdata_v, rtdata_v, lbdata_v)
         """
-        print "[ create_feeddata ]..."
+        print "[ create_feeddata hp]..."
         rows = [len(dataset)]
         bsdata = np.zeros(rows + list(dataset[0][0].shape))
         tsdata = np.zeros(rows + list(dataset[0][1].shape))
@@ -305,7 +306,7 @@ class DataManager():
         :param dataset: source dataset, a list of data cell of [bsdata, tsdata, rtdata, lbdata, tsdata_v, rtdata_v, lbdata_v]
         :return: tuple of (bsdata, tsdata, rtdata, lbdata, tsdata_v, rtdata_v, lbdata_v)
         """
-        print "[ create_feeddata ]..."
+        print "[ create_feeddata hp simple]..."
         rows = [len(dataset)]
         bsdata = np.zeros(rows + list(dataset[0][0].shape))
         tsdata = np.zeros(rows + list(dataset[0][1].shape))
@@ -351,7 +352,7 @@ class DataManager():
         rtlabels = ['code', 'open', 'high', 'trade', 'low', 'changepercent', 'turnoverratio']
         tsdataset = []
         rtdataset = []
-        print ("[ create today's dataset ]...")
+        print ("[ create today's dataset ]... for price prodiction")
 
         tsdata_dict = self.get_newly_data(days=22)
         rtdata_df = ts.get_today_all()[rtlabels]
@@ -362,7 +363,7 @@ class DataManager():
             tsdata_df= tsdata_dict[symb]
             rtdata_v = rtset[rtset[:,0] == int(symb)][0]
             if len(tsdata_df) >= lookback -1:
-                if rtdata_v[-2] > 9.98 or rtdata_v[-4] == 0: continue
+                if rtdata_v[-2] > 9.98 or rtdata_v[-1] == 0: continue
                 tsdata_v = np.vstack([np.array(tsdata_df), rtdata_v[1:]])[-lookback:]
                 ohcl = minmax_scale(tsdata_v[:,0:4])
                 tsdata = np.hstack([ohcl, tsdata_v[:, 4:]])
@@ -385,7 +386,7 @@ class DataManager():
         """
         rtlabels = ['code', 'open', 'high', 'trade', 'low', 'changepercent', 'turnoverratio']
         data_all = []
-        print ("[ create today's dataset ]...")
+        print ("[ create dataset ]... for validation day by day")
         sdate = datetime.date.today() - timedelta(days=30)
         sdate = sdate.strftime('%Y-%m-%d')
 
@@ -406,7 +407,7 @@ class DataManager():
             bsdata = bsset[bsset[:,0]==int(symb)][0]  # sym,...
             if len(tsdata_df) >= lookback + days:
                 dtcell = np.array(tsdata_df)[-lookback-days:]
-                ohcl = minmax_scale(dtcell[:,0:4])
+                ohcl = minmax_scale(dtcell[:,0:4]) ## TODO error here, not testcase level normalization
                 tsdata = np.hstack([datecol[-lookback-days:], ohcl, dtcell[:, 4:]])
                 tsdata_v = np.hstack([datecol[-lookback-days:], dtcell])
                 lbdata_v = np.hstack([np.zeros((days,1))+int(symb), datecol[-days:], dtcell[-days:]])

quantjia.py
@@ -44,6 +44,7 @@ def parse_params(mstr):
 
 def train_model_simple(mstr):
     params = parse_params(mstr)
+    print ("[ train model ]... " + mstr)
     dataset = dmr.create_dataset_simple(params['stocks'], params['lookback'])
     train, test = dmr.split_dataset(dataset, 0.75, params['batch_size'])
     bstrain, tstrain, lbtrain_v = dmr.create_feeddata_hp_simple(train)
@@ -83,8 +84,9 @@ def train_model_simple(mstr):
 
 
 def predict_today_simple(mstr):
+    print ("[ select stocks ]... using model:" + mstr)
     params = parse_params(mstr)
-    cust_objs = {params['custmetric']:eval('mdm.'+params['custmetric'])}
+    cust_objs = {params['custmetric']:eval('mdm.'+params['custmetric']), 'top_t1p1_class':mdm.top_t1p1_class}
     tsdata, rtdata_v = dmr.create_today_dataset_simple(params['lookback'])
     model = load_model('./models/'+params['model_name']+'/model.h5',custom_objects=cust_objs)
 
@@ -109,9 +111,35 @@ def predict_today_simple(mstr):
     return candidates
 
 
-def validate_model(mstr, days=8):
+def validate_model2(mstr, days=8):
+    print ("[ valid model: %s ]... with latest %s days data"%(mstr,str(days)))
     params = parse_params(mstr)
-    cust_objs = {params['custmetric']:eval('mdm.'+params['custmetric'])}
+    cust_objs = {params['custmetric']:eval('mdm.'+params['custmetric']), 'top_t1p1_class':mdm.top_t1p1_class}
+    model = load_model('./models/'+params['model_name']+'/model.h5',custom_objects=cust_objs)
+    valset = dmr.create_dataset_simple(3100, params['lookback'], days=days)
+    bsvalset, tsvalset, lbvalset_v = dmr.create_feeddata_hp_simple(valset)
+
+    test_x = tsvalset[:,:,1:]
+    test_y_v = lbvalset_v
+
+    if model.stateful:
+        test_x = test_x[:len(test_x) / params['batch_size'] * params['batch_size']]
+        test_y_v = test_y_v[:len(test_y_v) / params['batch_size'] * params['batch_size']]
+    proba = model.predict_proba(test_x, verbose=0, batch_size=params['batch_size'])
+
+    out = np.hstack([proba, test_y_v])
+    sortout = out[(-out[:, proba.shape[proba.ndim - 1] - 1]).argsort(), :]
+
+    if not __debug__:
+        np.savetxt("./models/" + params['model_name'] + "/val_newlydata_result.txt", sortout, fmt='%f')
+    else:
+        print sortout[0:200, :]
+    return sortout
+
+
+def validate_model(mstr, days=1):
+    params = parse_params(mstr)
+    cust_objs = {params['custmetric']:eval('mdm.'+params['custmetric']), 'top_t1p1_class':mdm.top_t1p1_class}
     bsdata, tsdata, tsdata_v, lbdata_v = dmr.create_val_dataset(params['lookback'], days=days)
     model = load_model('./models/'+params['model_name']+'/model.h5',custom_objects=cust_objs)
 
@@ -134,7 +162,9 @@ def validate_model(mstr, days=8):
 
 
 def _main_():
-    if(len(sys.argv) > 2):
+    if(len(sys.argv) > 3):
+        eval(sys.argv[1])(sys.argv[2], int(sys.argv[3]))
+    elif(len(sys.argv) > 2):
         eval(sys.argv[1])(sys.argv[2])
     else:
         eval(sys.argv[1])()